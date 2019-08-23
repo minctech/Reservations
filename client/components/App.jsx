e8d1191cd46c16d615d906aaeae1300a1a1a5860
John Yi <john.yi1411@gmail.com>
2019-08-23 10:54:55 -0700

    Add some initial styles. Implment app to pass previously written tests.

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,5 +1,45 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const App = () => <button type="button" onClick={() => console.log('what')}>Click</button>;
+const App = ({ listing }) => {
+  const chargePerNight = `$${listing.chargePerNight} per night`;
+  const review = `${listing.numberOfRatings}`;
+
+  return (
+    <div className="app">
+      <div>
+        <span>{chargePerNight}</span>
+        <span>{review}</span>
+      </div>
+    </div>
+  );
+};
+
+App.propTypes = {
+  listing: PropTypes.shape({
+    maxGuests: PropTypes.number,
+    maxInfants: PropTypes.number,
+    chargePerNight: PropTypes.number,
+    cleaningFee: PropTypes.number,
+    serviceFee: PropTypes.number,
+    occupancyFee: PropTypes.number,
+    rating: PropTypes.number,
+    numberOfRatings: PropTypes.number,
+  }),
+};
+
+App.defaultProps = {
+  listing: {
+    maxGuests: 5,
+    maxInfants: 3,
+    chargePerNight: 150,
+    cleaningFee: 10,
+    serviceFee: 10,
+    occupancyFee: 10,
+    rating: 9,
+    numberOfRatings: 300,
+  },
+};
+
 
 export default App;
--- client/index.jsx
+++ client/index.jsx
@@ -2,4 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+var listing = {
+  maxGuests: 5,
+  maxInfants: 3,
+  chargePerNight: 150,
+  cleaningFee: 10,
+  serviceFee: 10,
+  occupancyFee: 10,
+  rating: 9,
+  numberOfRatings: 300,
+};
+
+ReactDOM.render(<App listing={listing}/>, document.getElementById('root'));
