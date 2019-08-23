3dbc7ca57b6bdfa9dee7060f8204b9f3f0401d87
John Yi <john.yi1411@gmail.com>
2019-08-23 12:17:35 -0700

    Configure webpack.config to load files without extensions when importing

--- client/index.jsx
+++ client/index.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App.jsx';
 import axios from 'axios';
+import App from './components/App';
 
 axios.get('/api/dblistings', {
   params: {
     listing: 1,
-  }
+  },
 })
   .then((listing) => {
-    ReactDOM.render(<App listing={listing.data}/>, document.getElementById('root'));
+    ReactDOM.render(<App listing={listing.data} />, document.getElementById('root'));
   })
   .catch((error) => {
     console.log('error fetching listing data', error);
   });
-  
