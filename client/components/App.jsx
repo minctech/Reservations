f5fa28a1519b309d60155ec2fb8f8a0ed355666f
John Yi <john.yi1411@gmail.com>
2019-08-23 20:39:01 -0700

    Style out top static elements

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,45 +2,90 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
+const Flex = styled.div`
+  display: flex;
+`;
+
+const StarRatings = styled.span`
+  font-family: Circular, -apple-system, BlinkMacSystemFont, Roboto, "Helvetica Neue", sans-serif;
+  font-size 14px;
+  unicode-bidi: bidi-override;
+  color: gray;
+  height: 25px;
+  width: 62px;
+  position: relative;
+  margin-right: 5px;
+`;
+
+const TopStars = styled(StarRatings)`
+  color: teal;
+  position: absolute;
+  z-index: 1;
+  display: block;
+  top: 0;
+  left: 0;
+  overflow: hidden;
+`;
+
+const BottomStars = styled(StarRatings)`
+  padding: 0;
+  display: block;
+  z-index: 0;
+`;
+
+const OuterContainer = styled.div`
+  position: fixed;
+  top: 75px;
+  margin-left: 45px;
+  width: 376px;
+  border: 1px lightgray solid;
+  font-family: Circular, -apple-system, BlinkMacSystemFont, Roboto, "Helvetica Neue", sans-serif;
+`;
+
+const InnerContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 16px;
+  margin-bottom: 24px;
+  padding-left: 24px;
+  padding-right: 24px;
+`;
+
+const StaticWord = styled.span`
+  margin-right: 5px;
+  font-size 14px;
+`;
+
+const Cost = styled.span`
+  font-size: 22px;
+  font-weight: 700;
+  margin-right: 5px;
+`;
+
 
 const App = ({ listing }) => {
   const chargePerNight = `$${listing.chargePerNight}`;
   const review = `${listing.numberOfRatings}`;
-
-  const OuterContainer = styled.div`
-    position: fixed;
-    top: 75px;
-    margin-left: 45px;
-    width: 376px;
-    border: 1px lightgray solid;
-    font-family: Circular, -apple-system, BlinkMacSystemFont, Roboto, "Helvetica Neue", sans-serif;
-  `;
-
-  const InnerContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    margin-top: 16px;
-    margin-bottom: 24px;
-    padding-left: 24px;
-    padding-right: 24px;
-  `;
-
-  const Cost = styled.span`
-    font-size: 22px;
-    font-weight: 700;
-    margin-right: 5px;
-  `;
+  const width = `${listing.rating * 10}%`;
 
   return (
     <OuterContainer>
       <InnerContainer>
         <div>
           <Cost>{chargePerNight}</Cost>
-          <span>per night</span>
+          <StaticWord>per night</StaticWord>
         </div>
-        <div>
-          <span>{review}</span>
-        </div>
+        <Flex>
+          <StarRatings>
+            <TopStars style={{ width }}>
+              <span>★★★★★</span>
+            </TopStars>
+            <BottomStars>
+              <span>★★★★★</span>
+            </BottomStars>
+          </StarRatings>
+          <StaticWord>{review}</StaticWord>
+        </Flex>
       </InnerContainer>
     </OuterContainer>
   );
