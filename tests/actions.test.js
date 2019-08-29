b26c7daedafaa7afe0d52d3961c7d7d706099125
John Yi <john.yi1411@gmail.com>
2019-08-28 20:12:44 -0700

    Complete test coverage of all action creators

--- tests/actions.test.js
+++ tests/actions.test.js
@@ -1,8 +1,10 @@
 /* eslint-disable no-undef */
 import appActions from '../client/actions/App';
 import calendarActions from '../client/actions/Calendar';
+import calendarDateActions from '../client/actions/CalendarDates';
+import guestActions from '../client/actions/Guests';
 
-describe('appActions', () => {
+describe('App Actions', () => {
   it('should create an action to change listing', () => {
     const listing = {
       maxGuests: 5,
@@ -20,9 +22,36 @@ describe('appActions', () => {
     };
     expect(appActions.changeListing(listing)).toEqual(expectedAction);
   });
+
+  it('should create an action to change viewCalendar', () => {
+    const viewCalendar = true;
+    const expectedAction = {
+      type: 'CHANGE_VIEW_CALENDAR',
+      viewCalendar,
+    };
+    expect(appActions.changeViewCalendar(viewCalendar)).toEqual(expectedAction);
+  });
+
+  it('should create an action to change startDateView', () => {
+    const startDateView = true;
+    const expectedAction = {
+      type: 'CHANGE_START_DATE_VIEW',
+      startDateView,
+    };
+    expect(appActions.changeStartDateView(startDateView)).toEqual(expectedAction);
+  });
+
+  it('should create an action to change guestContainerView', () => {
+    const guestContainerView = true;
+    const expectedAction = {
+      type: 'CHANGE_GUEST_CONTAINER_VIEW',
+      guestContainerView,
+    };
+    expect(appActions.changeGuestContainerView(guestContainerView)).toEqual(expectedAction);
+  });
 });
 
-describe('calendarActions', () => {
+describe('Calendar Actions', () => {
   it('should create an action to change month', () => {
     const expectedAction = {
       type: 'CHANGE_CURRENT_MONTH',
@@ -37,5 +66,82 @@ describe('calendarActions', () => {
       currentYear: 2020,
     };
     expect(calendarActions.changeCurrentYear(2020)).toEqual(expectedAction);
+  });
+});
+
+describe('CalendarDates Actions', () => {
+  it('should create an action to change selectedStartDate', () => {
+    const selectedStartDate = {
+      year: 2019,
+      month: 8,
+      day: 10,
+    };
+    const expectedAction = {
+      type: 'CHANGE_START_DATE',
+      selectedStartDate,
+    };
+    expect(calendarDateActions.changeSelectedStartDate(selectedStartDate)).toEqual(expectedAction);
+  });
+
+  it('should create an action to change selectedEndDate', () => {
+    const selectedEndDate = {
+      year: 2019,
+      month: 8,
+      day: 10,
+    };
+    const expectedAction = {
+      type: 'CHANGE_END_DATE',
+      selectedEndDate,
+    };
+    expect(calendarDateActions.changeSelectedEndDate(selectedEndDate))
+      .toEqual(expectedAction);
+  });
+
+  it('should create an action to change selectedDates', () => {
+    const selectedDates = [{}, {}];
+    const expectedAction = {
+      type: 'CHANGE_SELECTED_DATES',
+      selectedDates,
+    };
+    expect(calendarDateActions.changeSelectedDates(selectedDates))
+      .toEqual(expectedAction);
+  });
+});
+
+describe('Guests Actions', () => {
+  it('should create an action to change selectedAdults', () => {
+    const selectedAdults = 3;
+    const expectedAction = {
+      type: 'CHANGE_SELECTED_ADULTS',
+      selectedAdults,
+    };
+    expect(guestActions.changeSelectedAdults(selectedAdults)).toEqual(expectedAction);
+  });
+
+  it('should create an action to change selectedChildren', () => {
+    const selectedChildren = 3;
+    const expectedAction = {
+      type: 'CHANGE_SELECTED_CHILDREN',
+      selectedChildren,
+    };
+    expect(guestActions.changeSelectedChildren(selectedChildren)).toEqual(expectedAction);
+  });
+
+  it('should create an action to change selectedInfants', () => {
+    const selectedInfants = 3;
+    const expectedAction = {
+      type: 'CHANGE_SELECTED_INFANTS',
+      selectedInfants,
+    };
+    expect(guestActions.changeSelectedInfants(selectedInfants)).toEqual(expectedAction);
+  });
+
+  it('should create an action to change maxGuestsReached', () => {
+    const maxGuestsReached = true;
+    const expectedAction = {
+      type: 'CHANGE_MAX_GUESTS_REACHED',
+      maxGuestsReached,
+    };
+    expect(guestActions.changeMaxGuestsReached(maxGuestsReached)).toEqual(expectedAction);
   });
 });
