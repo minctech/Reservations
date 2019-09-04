ed90b57cc0c2fec71e66c0f1f1944c76270b0e12
John Yi <john.yi1411@gmail.com>
2019-09-03 19:34:48 -0700

    Remove commented out lines in index.

--- client/index.jsx
+++ client/index.jsx
@@ -5,7 +5,6 @@ import { Provider } from 'react-redux';
 import AppContainer from './containers/AppContainer';
 import store from './store/store';
 import appActions from './actions/App';
-// import Main from './components/Main';
 
 class Reservations extends React.Component {
   componentDidMount() {
@@ -43,39 +42,6 @@ class Reservations extends React.Component {
     );
   }
 }
-
-// ReactDOM.render(
-//   <Provider store={store}>
-//     <AppContainer />
-//   </Provider>,
-//   document.getElementById('root'),
-//   () => {
-//     axios.get('/api/dblistings', {
-//       params: {
-//         listing: document.URL.split('/').reverse()[1],
-//       },
-//     })
-//       .then((listing) => {
-//         store.dispatch(appActions.changeListing(listing.data));
-//       })
-//       .then(() => {
-//         axios.get('/api/dbbookeddates', {
-//           params: {
-//             listing: document.URL.split('/').reverse()[1],
-//           },
-//         })
-//           .then((bookedDates) => {
-//             store.dispatch({
-//               type: 'CHANGE_BOOKED_DATES',
-//               bookedDates: bookedDates.data,
-//             });
-//           });
-//       })
-//       .catch((error) => {
-//         console.log('error fetching listing data', error);
-//       });
-//   },
-// );
 
 document.addEventListener('click', (e) => {
   if (e.target.tagName !== 'svg' && e.target.tagName !== 'path') {
